3afe87bb3347b6c3e3f8f0f3e7c247d7a70f78be
Karimkh31 <58221455+Karimkh31@users.noreply.github.com>
2021-04-26 12:30:05 +0200

    added LoadingSpinner.js

---
+++ src/Design/LoadingSpinner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LoadingSpinner.css";
+
+
+
+const LoadingSpinner = () => {
+    return(
+        <div className="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    );
+};
+
+export default LoadingSpinner;
