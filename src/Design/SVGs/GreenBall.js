cdf0d453b56a1b2cde071a8154609b6f8a75017f
Chantal Rufer <vukii@yahoo.com>
2021-04-27 19:37:07 +0200

    Red and Green Ball

---
+++ src/Design/SVGs/GreenBall.js
@@ -0,0 +1,8 @@
+const GreenBall = () => {
+    return(
+        <img width="20" alt="Green sphere" src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Green_sphere.png"/>
+    );
+};
+
+export default GreenBall
+
---
+++ src/Design/SVGs/RedBall.js
@@ -0,0 +1,7 @@
+const RedBall = () => {
+    return(
+        <img width="18" alt="Offline dot" src="https://upload.wikimedia.org/wikipedia/commons/8/80/Offline_dot.png"/>
+    );
+};
+
+export default RedBall
